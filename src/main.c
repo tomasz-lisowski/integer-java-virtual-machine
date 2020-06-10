2414ff3addb5991eb4ca7e32051181003c110536
Tomasz Lisowski <t.p.lisowski@gmail.com>
2020-06-10 20:36:42 +0200

    Show time taken to execute

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
+#include <time.h>
+
+
 #include "init.h"
 #include "terminate.h"
 #include "interpreter.h"
+#include "util.h"
+
+
+clock_t start, end;
+double cpu_time_used;
 
 
 void print_help()
@@ -10,6 +18,7 @@ void print_help()
 
 int main(int argc, char** argv)
 {
+    start = clock();
     if (argc < 2)
     {
         print_help();
@@ -26,5 +35,8 @@ int main(int argc, char** argv)
 
     destroy_ijvm();
 
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    fprintf(stderr, "[Took %f seconds]\n", cpu_time_used);
     return 0;
 }
