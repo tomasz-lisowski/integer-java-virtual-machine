10d0a2620d320de836b7e38a03f58eea64c6ba27
Tomasz Lisowski <t.p.lisowski@gmail.com>
2020-06-16 21:18:54 +0200

    Improve usage msg and let in/out files to be passed via cmd line args

--- src/main.c
+++ src/main.c
@@ -7,9 +7,10 @@
 #include "util.h"
 
 
-static void print_help(void)
+static void print_usage(void)
 {
-    printf("Usage: ./ijvm binary \n");
+    printf("Integer Java Virtual Machine\n");
+    printf("Usage: ./ijvm <path/to/binary.ijvm> [<in_file>] [<out_file>]\n");
 }
 
 
@@ -21,7 +22,7 @@ int main(int argc, char** argv)
 
     if (argc < 2)
     {
-        print_help();
+        print_usage();
         return 1;
     }
 
@@ -29,6 +30,29 @@ int main(int argc, char** argv)
     {
         fprintf(stderr, "Couldn't load binary %s\n", argv[1]);
         return 1;
+    }
+
+    if (argc >= 3)
+    {
+        FILE* in_file = fopen(argv[2], "r");
+        if (in_file == NULL)
+        {
+            fprintf(stderr, "Couldn't open %s\n", argv[2]);
+            return -1;
+        }
+        set_input(in_file);
+        
+    }
+
+    if (argc == 4)
+    {
+        FILE* out_file = fopen(argv[3], "w+");
+        if (out_file == NULL)
+        {
+            fprintf(stderr, "Couldn't open %s\n", argv[3]);
+            return -1;
+        }
+        set_output(out_file);
     }
 
     run();
